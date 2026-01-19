d2c2ec6fa9e436eadebfdbd98e8b30869abac2bf
juliano-CN <juliano.nagy@hotmail.com>
2026-01-19 16:06:42 -0300

    requirements.txt

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import CollectAllData
 import CollectAllDataReviews
 
 def load_data():
-    games_list = pd.read_csv("Data/appidsList.csv",sep = ",")#carrega a lista de appids(5 de janeiro de 2026)
-    appid = games_list["appid"][145000:151975]#1150
+    games_list = pd.read_csv("Data/appidsList.csv",sep = ",")
+    appid = games_list["appid"]
 
-    #CollectAllData.getData(appid)
+    CollectAllData.getData(appid)
     CollectAllDataReviews.getData(appid,wait_time=1)
 
 if __name__ == "__main__":
